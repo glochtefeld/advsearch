9c153c13288d08441e5516b2beaa5795da56b4c6
Gavin Lochtefeld <gavin.loch@gmail.com>
2022-10-31 15:46:45 -0500

    finished back half of the implementation

--- advsearch.js
+++ advsearch.js
@@ -1,79 +1,157 @@
 /* Grammar
  * search   ->  term* END
- * term     ->  label ':' match
- * match    ->  quote STRING quote
+ * term     ->  single | pair
+ * pair     ->  '@' label ':' ( single | quotestr )
+ * quotestr ->  quote STRING quote
  * quote    ->  " | '
  * label    ->  [a-z_]+
- * STRING   ->  [%A-Za-z0-9 ]+
+ * single   ->  [A-Za-z0-9]+
+ * STRING   ->  [A-Za-z0-9\s]+ // Until end quote
  */
 
-const labels = {
-    id: 0,
+let labels = { // Given by backend
+    any: 0,
     product_code: 1,
     description: 2,
     alt_description: 3,
 };
-const types = [
-    label: 0,
-    string: 1
-];
+let types = { // Given by backend
+    string: 1,
+    number: 2
+};
 
 const getlabel = (l) => labels[l] || console.error('No such label ' + l);
+const gettype = (t) => types[t] || console.error('No such type ' + l);
 const scanner = function(str) {
     let idx = 0;
     const advance = ()=> str[idx++];
     const current = ()=> str[idx];
-    const peek = ()=> idx + 1 > str.length ? -1 : str[idx + 1];
+    const previous = () => str[idx - 1];
+    const peek = ()=> idx + 1 >= str.length ? -1 : str[idx + 1];
     const skipWhitespace = ()=> {
-        while (current().match(/\s/)) advance();
+        while (peek() != -1 && current().match(/\s/)) advance();
     };
     const makeToken = (tokType)=> {
-        let token = {
-            type: gettype(tokType),
-            data: '',
-        };
+        //console.log('   tokType: ' + tokType);
+        let data = previous();
+        let type = 0;
+        let exact = 0;
         switch (tokType) {
-            case 'label':
-                while (current().match(/[a-z_]/)) {
-                    token.data += current();
-                    advance();
-                }
-                getlabel(tokType);
-                break;
             case 'STRING':
+                data = ''; // skip opening quote
+                exact = 1;
                 while (current().match(/[^\'\"]/) && peek() != -1) {
-                    token.data += current();
-                    advance();
+                    data += advance();
                 }
-                if (peek() == -1) 
+                if (peek() == -1 && current().match(/[^\'\"]/))
+                    throw "Quotes must have a closing pair.";
+                advance(); // Skip closing quote
                 break;
+            case 'WORD':
+                while ( current().match(/\S/) && peek() != -1 ) {
+                    data += advance();
+                    //console.log('   chr ' + idx + '/' + str.length);
+                    //console.log('   data: ' + data + ', current='+current() + ', peek='+peek());
+                }
+                if (peek() == -1) data += advance();
+                break;
+            case 'END':
+                //console.log('finished');
+                return [];
+            case 'label':
+                data = '';
+                while (current().match(/[^\:]/) && peek() != -1)
+                    data += advance();
+                advance(); // Skip ':'
+                //console.log('   Label: ' + data + ' (' + getlabel(data) + ')');
+                return [getlabel(data)];
             default:
-                token.data = null;
                 break;
         }
-        return token;
+        if (data.match(/^[\d\.]+$/) && tokType != 'STRING') {
+            type = gettype('number');
+            data = Number(data);
+            exact = 1;
+        } else type = gettype('string');
+        //console.log('   data: ' + data + ' type: ' + type);
+
+        return [data, type, exact];
     };
 
     const scanTok = ()=> {
         skipWhitespace();
-        if (peek() == -1) return makeToken('END');
+        if (peek() == -1) { /*console.log('scanned to end');*/ return makeToken('END'); }
         let c = advance();
+        //console.log('Scanned ' + c);
         switch (c) {
             case '"': 
             case "'": return makeToken('STRING');
-            case ':': return makeToken('COLON');
+            case '@': return makeToken('label');
             default:
-                makeToken('label');
+                return makeToken('WORD');
         }
     };
+    const scan = ()=> {
+        let parser = {
+            previous: [],
+            current: [],
+            code: []
+        };
+        const printparser = ()=> {
+            console.log('previous:['+parser.previous+ '] current:['+parser.current+']');
+        }
+        const printCode = ()=> {
+            console.log('parser code:['+parser.code+']');
+        }
 
+        const advance = ()=> { 
+            parser.previous = parser.current;
+            parser.current = scanTok();
+        }
+        const check = (t)=> parser.current.length == t;
+
+        const match = (t)=> { 
+            if (!check(t)) return false; 
+            advance(); 
+            return true; 
+        };
+        const matchEOF = ()=> match(0);
+        const checkTerm = ()=> check(3);
+
+        advance();
+        while (!matchEOF()) {
+            //printparser();
+            //printCode();
+            //console.log('cur_is_data:'+check(2) + ' prevlen:' + parser.previous.length);
+            if (checkTerm() && parser.previous.length != 1) // No label passed previously
+                parser.code.push(0);
+
+            parser.code.push(...parser.current);
+            advance();
+        }
+        //printCode();
+        if (parser.code.length % 4 != 0) throw 'Incorrect code generated.';
+        return parser.code;
+    }
+    return scan();
 };
 
 const compile = function(str) {
     try {
+        console.log(str);
         let terms = scanner(str);
+        console.log(terms);
     } catch (err) {
         console.error(err);
     }
 };
-    
+
+compile("'tomato' @product_code:11 paste");
+compile("@product_code:'Fresh' @description:Meaty");
+//compile("@product_code:'Fresh @description:Meaty"); // Fail
+
+/* Example transformation:
+ * tomato @product_code:'11' paste -> 0 'tomato' 1 1 '11' 1 0 'paste' 1
+ * which turns into a query like "select * from TBL where product_code='11' and ( description like '%tomato%' or alt_description like '%tomato%' )"
+ * Noting that the 'any' list did not include specified terms. Specified terms are conjunctive (and), while 'any' terms are internally disjunctive (or).
+ */
--- advsearch.php
+++ advsearch.php
@@ -1,30 +1,77 @@
 <?php
 
 class SearchTerm {
-    public String $label;
     public String $dbfield;
     public bool $exact;
     public $search;
-    public function substitute() {}
+    public $type;
+    public function __construct(Int $label, $data, Int $type, Bool $exact) {
+        $this->dbfield = array_values($searchTerms)[$label];
+        $this->search = $data;
+        $this->exact = $exact;
+        if (!$this->exact)
+            $this->search = '%'.$this->search.'%';
+        $this->type = $searchTypes[$type];
+    }
+    public function substitute() {
+        if ($this->dbfield !== 0) {
+            return $this->dbfield 
+                . ($this->exact ? '=' : ' like ') 
+                . ($this->type == 'i' ? '?' : "'". $this->exact ?"'"));
+        }
+    }
 }
+
+$searchTerms = [
+    'any'=>0,
+    'product_code'=>'IPP_ProductCode',
+    'description'=>'IPP_Description',
+    'alt_description'=>'IPP_DescriptionAlt'
+];
+$searchTypes = [ 's', 'i' ];
 
 class Searcher extends DatabaseCaller {
     private $terms; // SearchTerm[]
-    public function interpret(String $terms) {
-        $this->terms = $this->generateTerms($terms);
+    private $specified;
+    private $anyTerms;
+    public function interpret($terms) {
+        $this->generateTerms($terms);
+        return $this->search();
     }
-    private function generateTerms(String $terms) {
+    private function generateTerms($terms) {
+        $params = array_chunk($terms, 4);
+        foreach ($params as $p) {
+            $t = new SearchTerm($p[0], $p[1], $p[2], $p[3]);
+            if ($p[0]) $this->terms []= $t;
+            else $this->anyTerms []= $t
+        }
     }
     private function search() {
         $sql = 'select * 
             from TBL
             where ';
+        $any = '(';
         $params = [];
+
         foreach ($this->terms as $t) {
             $sql .= $t->substitute();
+            if ($t->exact) $this->specified[]=$t->dbfield;
             $params []= $t->search;
             if ($t !== array_key_last($this->terms)) $sql .= ' and ';
         }
+        if (sizeof($this->anyTerms)) {
+            $searches = array_diff(array_slice(array_values($searchTerms), 1), $this->specified);
+            foreach ($this->anyTerms as $t) {
+                foreach ($searches as $s) {
+                    $any .= $s.' like '.$t->search;
+                    $params []= $t->search;
+                    if ($t !== array_key_last($this->anyTerms)) $any.=' or ';
+                }
+            }
+            $any.=')';
+            $sql.=' and '.$any;
+        }
+        
         $res = $this->db->retrieve($sql, $params);
         $this->escapeIfNull($res, 'Could not complete search', false);
         return $res;
